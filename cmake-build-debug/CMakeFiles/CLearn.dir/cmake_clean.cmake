5d613a15bc56747d11191052cb75dc8149baf86b
waver <jokerwaver@gmail.com>
2019-02-13 00:20:06 +0800

    第二天提交

---
+++ cmake-build-debug/CMakeFiles/CLearn.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CLearn.dir/chapter1/main.c.o"
+  "CMakeFiles/CLearn.dir/chapter1/fahrenheit.c.o"
+  "CLearn.pdb"
+  "CLearn"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CLearn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/power.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/power.dir/chapter1/power.c.o"
+  "power.pdb"
+  "power"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/power.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
